84047ffe4b5254c2c5d94d562d15063db8e85c3c
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-06-20 15:10:35 +0600

    all routes done

--- routes/users.js
+++ routes/users.js
@@ -1,2 +1,19 @@
 import express from "express";
-import { getUser, GetUserFriends, addRemoveFriend } from "../controllers/users";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+
+router.patch("/:id/:friend", verifyToken, addRemoveFriend);
+
+export default router;
